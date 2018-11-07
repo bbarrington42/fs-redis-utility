3489a4fda494999435795f1ed58496294575b758
Bill Barrington <bill.barrington@candidpartners.com>
2018-11-07 16:38:03 -0500

    First steps toward implementing list command.

--- src/main/scala/ccfs/Main.scala
+++ src/main/scala/ccfs/Main.scala
@@ -1,7 +1,14 @@
 package ccfs
 
 import com.typesafe.config.ConfigFactory
-import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
+import redis.clients.jedis._
+import scalaz.Monoid
+
+import scala.annotation.tailrec
+
+import scala.collection.JavaConverters._
+
+import scalaz.std.list._
 
 object Main {
 
@@ -11,12 +18,41 @@ object Main {
 
   val jedisPool = new JedisPool(poolConfig, config.getString("host"), config.getInt("port"))
 
-  def connect(jedis: Jedis): Unit = {
 
+
+  val DISPENSER_KEY_PREFIX = "dispenser:"
+
+  // Generalize operations requiring a scan of all keys matching a pattern followed by a transformation of the result.
+  // The result must be a Monoid.
+  def scan[T](jedis: Jedis, keyPattern: String, xform: (ScanResult[String], Jedis) => T)(implicit m: Monoid[T]): T = {
+    val params = new ScanParams
+    params.`match`(keyPattern)
+
+    @tailrec
+    def _scan(result: ScanResult[String], acc: T): T = {
+      val t = xform(result, jedis)
+      val nextCursor = result.getStringCursor
+      if (ScanParams.SCAN_POINTER_START == nextCursor) m.append(acc, t)
+      else _scan(jedis.scan(nextCursor, params), m.append(acc, t))
+    }
+
+    _scan(jedis.scan(ScanParams.SCAN_POINTER_START, params), m.zero)
+  }
+
+  def collect(result: ScanResult[String], jedis: Jedis): List[String] = {
+    result.getResult.asScala.toList
+  }
+
+  def list(jedis: Jedis): List[String] = {
+    scan(jedis, DISPENSER_KEY_PREFIX, collect)
   }
 
   def main(args: Array[String]): Unit = {
-    withJedis(jedisPool)(connect)
+    withJedis(jedisPool)(jedis => {
+      val listing = list(jedis)
+
+      println(listing)
+    })
   }
 
 }

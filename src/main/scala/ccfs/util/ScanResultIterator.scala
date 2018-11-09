101f28749252665c31e03262ed0eab9aa7de142e
Bill Barrington <bill.barrington@candidpartners.com>
2018-11-09 07:41:14 -0500

    Fixed initialization bug in iterator.

--- src/main/scala/ccfs/util/ScanResultIterator.scala
+++ src/main/scala/ccfs/util/ScanResultIterator.scala
@@ -7,21 +7,16 @@ class ScanResultIterator(jedis: Jedis, pattern: String) extends Iterator[Result]
   val params = new ScanParams
   params.`match`(pattern)
 
-  private var result: Option[Result] = None
+  private var result: Result = jedis.scan(ScanParams.SCAN_POINTER_START, params)
 
-  override def hasNext: Boolean =
-    result.map(res => ScanParams.SCAN_POINTER_START != res.getStringCursor).getOrElse(true)
+  override def hasNext: Boolean = ScanParams.SCAN_POINTER_START != result.getStringCursor
 
-  override def next(): Result = result match {
-    case Some(res) =>
-      result = Option(jedis.scan(res.getStringCursor, params))
-      res
-
-    case None =>
-      val res = jedis.scan(ScanParams.SCAN_POINTER_START, params)
-      result = Option(res)
-      res
+  override def next(): Result = {
+    val rv = result
+    result = jedis.scan(result.getStringCursor, params)
+    rv
   }
+
 }
 
 object ScanResultIterator {

392297a33a634e40f86cf1a572e6f314f732ce7c
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-18 11:00:37 -0500

    Added service to execute external command. Not currently used.

--- src/main/scala/ccfs/Main.scala
+++ src/main/scala/ccfs/Main.scala
@@ -26,17 +26,28 @@ object Main {
 
       println(s"# keys: ${keys.length}")
 
-      println(keys.mkString("\n"))
+      //println(keys.mkString("\n"))
+
+
+      val props = sessionProps(jedis)
+        //zplProps(jedis)
+
+      val s = "9736AEEA-26F2-9652-1C9C-EDA87AF402E5"
+      println(s"session: $s, ${props.matches(s)}")
+
+//      val zpl = "ZPL10011CE"
+//
+//      println(s"zpl: $zpl, ${props.matches(zpl)}")
 
 //      val n = delKeys(jedis)
 //
 //      println(s"Deleted: $n keys")
 
-//      val props = zplProps(jedis)
+      //val props = zplProps(jedis)
 //
 //      println(props)
 
-//      println(props.matches("ZPL199137J"))
+      //println(props.matches("ZPL3205270"))
 //
 //      println(props.map.keys)
       //      val r = matches(map, "ZPL100493W")
